e012c73fd76e75bad89ecfe9706ebae76cc9d766
ksh103 <jayy_z@naver.com>
2022-10-16 13:16:46 +0900

    Solved 프로그래머스 - 짝지어 제거하기

---
+++ programmers/Level2/짝지어제거하기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        
+        char[] chArr = s.toCharArray();
+        
+        for(char ch : chArr) {
+            // 스택이 비어 있다면 문자열 추가
+            if(stack.isEmpty()) {
+                stack.push(ch);
+            } else {
+                // 스택에 있는 문자열이 비교하고자 하는 문자열과 같을 때
+                if(stack.peek() == ch) {
+                    // 해당 문자열 제거
+                    stack.pop();
+                } else {
+                    // 문자열 추가
+                    stack.push(ch);
+                }
+            }
+        }
+
+        int answer = -1;
+        
+        if(stack.isEmpty()) answer = 1;
+        else answer = 0;
+        
+        return answer;
+    }
+}
